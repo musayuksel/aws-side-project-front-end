01cc32f1c3ad16f6f6924506a08c6cc0aaac0b45
Musa <79163385+musayuksel@users.noreply.github.com>
2024-12-25 18:05:02 +0000

    Refactor CategoryFilterTabs styles and structure; enhance tab focus behavior and implement active tab state management

--- src/components/CategoryFilterTabs/CategoryFilterTabs.jsx
+++ src/components/CategoryFilterTabs/CategoryFilterTabs.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import "./CategoryFilterTabs.css";
 
+const categories = ["Most Popular", "Sport", "Live"];
+
 export const CategoryFilter = ({ focusedIndex, setTabRef, focusedSection }) => {
-  const categories = ["Popular", "Sport", "Live"];
+  const [activeTab, setActiveTab] = useState(focusedIndex || 0);
+
+  const handleClick = (event, tabText) => {
+    event.preventDefault();
+    setActiveTab(tabText);
+  };
+  console.log({ activeTab });
 
   return (
     <div className="category-filter-container">
       {categories.map((tab, index) => (
         <a
           key={index}
-          className="tab"
+          className={`tab ${activeTab === tab ? "active" : ""}`}
           ref={(element) => setTabRef(element, index)}
           tabIndex={
             focusedSection === "tabs" && focusedIndex === index ? 0 : -1
           }
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleClick(event, tab);
+            }
+          }}
+          href="#"
         >
           {tab}
         </a>
