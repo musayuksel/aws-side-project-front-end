490c7d444f2a9f933d6312766c007f731c54f69b
Musa <79163385+musayuksel@users.noreply.github.com>
2025-02-26 15:41:42 +0000

    Update CI pipeline to use main template for CloudFormation stack deployment

--- ci/scripts/update-ci-pipeline.sh
+++ ci/scripts/update-ci-pipeline.sh
@@ -4,7 +4,7 @@ set -e
 echo "Creating/Updating CloudFormation stack: musa-yuksel-cloudfront-test-stack-2 in eu-west-1"
 
 aws cloudformation deploy \
-  --template-file ../simple-s3-test-stack.yml \
+  --template-file ../mainTemplate.yml \
   --stack-name musa-yuksel-cloudfront-test-stack-2 \
   --capabilities CAPABILITY_IAM \
   --region eu-west-1
