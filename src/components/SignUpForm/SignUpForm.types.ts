0b6ccce94bac3eaa7ae8ea1260a4c4ebd6f329f7
Musa <79163385+musayuksel@users.noreply.github.com>
2025-03-19 12:17:24 +0000

    Remove SignUpForm component and related types

--- src/components/SignUpForm/SignUpForm.types.ts
+++
@@ -1,6 +0,0 @@
-import { SignUpUserWithCognitoArgsTypes } from '../../utils';
-
-export type TSignUpFormProps = {
-  userInfos: SignUpUserWithCognitoArgsTypes;
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
